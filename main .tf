5abbd26f008268184d06bd9427ff501a6a2c443f
dozman <chiedozieonyekwum.com>
2022-08-03 14:34:00 -0500

    initial commit

---
+++ main .tf
@@ -0,0 +1,21 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+# Install the ECK operator
+resource "helm_release" "elastic-system" {
+  name       = "elastic-system"
+  repository = "https://helm.elastic.co"
+  chart      = "eck-operator"
+  namespace  = "default"
+}
+
+
+resource "helm_release" "assignment" {
+  name      = "assign-chart_latest"
+  chart     = "./KandE"
+  namespace = "default"
+
+}
